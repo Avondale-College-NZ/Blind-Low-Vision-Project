973bfe9caa1f9c4b73346043ff09d3b1f4253536
Nikhil Singh <ac98926@avcol.school.nz>
2020-11-04 09:17:05 +1300

    added a new page

--- AuthSystem/Controllers/HomeController.cs
+++ AuthSystem/Controllers/HomeController.cs
@@ -48,6 +48,11 @@ namespace AuthSystem.Controllers
             return View();
         }
 
+        public IActionResult SubmissionSuccessful()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
